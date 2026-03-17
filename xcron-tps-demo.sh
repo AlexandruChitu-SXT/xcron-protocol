dfae456cfa70fd6c63bb3d1b57c3ab5b917ac541
xcronprotocol <xcronprotocol@users.noreply.github.com>
2026-03-17 02:22:08 +0100

    fix(rust): Repoint TPS demo specifically to BoN Devnet infrastructure

--- xcron-tps-demo.sh
+++ xcron-tps-demo.sh
@@ -61,6 +61,6 @@ echo -e "\n${GREEN}🚀 IGNITION! Executing Tokio Tasks...${NC}"
     --tps 50000 \
     --broadcasters 2500 \
     --wallets 500 \
-    --gateway "https://testnet-api.multiversx.com" \
-    --chain-id "T" \
+    --gateway "https://devnet-api.multiversx.com" \
+    --chain-id "D" \
     --wallets-file "../.secrets/hydra-keys.json"
